cde1251ef3f42c5c7a2b2e73f79d07d87fd7bcc2
SPyaePhyoThu <satwotwo@gmail.com>
2023-12-14 16:54:24 +0630

    delete sharp

--- backend/server/controller/userController.js
+++ backend/server/controller/userController.js
@@ -1,5 +1,4 @@
 const multer = require("multer");
-const sharp = require("sharp");
 const gm = require("gm");
 const User = require("../models/userModel");
 const catchAsync = require("../util/catchAsync");
