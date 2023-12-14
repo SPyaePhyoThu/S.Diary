8cb4c1b55e8311b58bca88765b3af7b132cbe9a8
SPyaePhyoThu <satwotwo@gmail.com>
2023-12-14 20:02:40 +0630

    add MethodNotAllowed MW

--- backend/api/routes/userRoutes.js
+++ backend/api/routes/userRoutes.js
@@ -3,8 +3,17 @@ const authController = require("../controller/authController");
 const userController = require("../controller/userController");
 
 const router = express.Router();
+const methodNotAllowed = (req, res, next) => res.status(405).send();
+router.route("/login").post(authController.login).all(methodNotAllowed);
 
-router.post("/login", authController.login);
+app.use((req, res, next) => {
+  const methods = router.stack
+    // Filter for the route that matches the currently matched route
+    .filter((layer) => layer.route.path === req.path)[0].route.methods;
+  if (!methods[req.method]) methodNotAllowed(req, res, next);
+  else next();
+});
+
 router.post("/signup", authController.signup);
 router.get("/logout", authController.logout);
 
