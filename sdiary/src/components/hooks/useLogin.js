188406241a84b8673e1e1a17767d88db2ce06f7d
SPyaePhyoThu <satwotwo@gmail.com>
2023-12-15 11:54:18 +0630

    changed URL

--- sdiary/src/components/hooks/useLogin.js
+++ sdiary/src/components/hooks/useLogin.js
@@ -11,7 +11,7 @@ export const useLogin = () => {
     setError(null);
 
     try {
-      const response = await fetch("http://127.0.0.1/api/v1/user/login", {
+      const response = await fetch("/api/v1/user/login", {
         method: "POST",
         mode: "cors",
         // headers: { "Content-Type": "application/json" },
